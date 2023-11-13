d08f9cf601f5e40dad152ce7b34e4f2852161b99
vini <velkhaliliborsato@gmail.com>
2023-11-12 23:54:51 -0300

    index page link

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { gsap, Power3 } from "gsap"
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <Link href={"/tutorial01"}>Tutorial 01</Link>
     </main>
   )
 }
