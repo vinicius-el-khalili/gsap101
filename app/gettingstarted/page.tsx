93403c999e777026c04d6895e8a81074789d7dcc
vini <velkhaliliborsato@gmail.com>
2023-11-14 15:52:54 -0300

    understanding Tweens

--- app/gettingstarted/page.tsx
+++ app/gettingstarted/page.tsx
@@ -1,11 +1,11 @@
-import style from "@/sass/pages/GettingStarted.module.scss"
+import style from "@/sass/pages/Tutorial02.module.scss"
 import ReRenderable from "@/components/Tutorial01/ReRenderable";
 import CreatingAnAnimation from "@/components/Tutorial02/CreatingAnAnimation";
 const GettingStartedPage = () => {
     return (
         <div className={style.GettingStartedPage}>
             <ReRenderable>
-                <CreatingAnAnimation/>
+                <CreatingAnAnimation/>  
             </ReRenderable>
         </div>
     );
--- components/Tutorial02/CreatingAnAnimation.tsx
+++ components/Tutorial02/CreatingAnAnimation.tsx
@@ -2,6 +2,7 @@
 
 import { useRef, useLayoutEffect } from "react";
 import gsap from "gsap";
+import us from "@/sass/utilityStyles/utilityStyles.module.scss"
 
 const CreatingAnAnimation = () => {
 
@@ -9,15 +10,52 @@ const CreatingAnAnimation = () => {
 
     useLayoutEffect(()=>{
         const context = gsap.context(()=>{
-
+            gsap.to(".boxTo",{x:100})
+            gsap.from(".boxFrom",{x:100})
+            gsap.fromTo(".boxFromTo",{x:-100},{x:100})
+            gsap.set(".boxSet",{x:50})
+            gsap.fromTo(".wtf",
+            {
+                width:0,
+                height:0,
+                fontSize:18,
+                borderRadius:0,
+                backgroundColor:"blue",
+                duration:1.5
+            },
+            {
+                width:150,
+                height:150,
+                fontSize:52,
+                borderRadius:"100%",
+                backgroundColor:"red",
+                rotate: 720,
+                duration: 1.5
+            })
         },rootRef)
+        return ()=>{
+            context.revert()
+        }
     },[])
 
     return (
         <>
-        <h1>Creating animations</h1>
+        <h1>Tweens</h1>
         <div ref={rootRef}>
 
+            <p>{`default`}</p>
+            <div className={`${us.box}`}/>
+            <p>{`to {x:100}`}</p>
+            <div className={`${us.box} boxTo`}/>
+            <p>{`from {x:100}`}</p>
+            <div className={`${us.box} boxFrom`}/>
+            <p>{`fromTo {x:-100} {x:100}`}</p>
+            <div className={`${us.box} boxFromTo`}/>
+            <p>{`set {x:100}`}</p>
+            <div className={`${us.box} boxSet`}/>
+
+            <h1>Let's go crazy</h1>
+            <div className="wtf">WTF DUDE</div>
         </div>
         
         </>
