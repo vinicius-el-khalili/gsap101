305d2d588c4089dc9bd4c6233f0526a644637a89
vini <velkhaliliborsato@gmail.com>
2023-11-16 12:07:53 -0300

    testing alphabets

---
+++ app/alphabets/alphabet01/page.tsx
@@ -0,0 +1,13 @@
+import Alphabet01 from "@/components/Alphabet/Alphabet01";
+
+const Alphabet01Page = () => {
+    return (
+        <>
+        
+        <Alphabet01/>
+        
+        </>
+    );
+}
+ 
+export default Alphabet01Page;
