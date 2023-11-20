9668620fa52b04f496426202dc9a50980778f456
vini <velkhaliliborsato@gmail.com>
2023-11-19 21:10:16 -0300

    works on chrome but not in firefox. deploying

--- components/rounded-text-cursor/RoundedText.tsx
+++ components/rounded-text-cursor/RoundedText.tsx
@@ -1,33 +1,34 @@
 "use client"
 import gsap from "gsap";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import style from "@/sass/components/rounded-text-cursor/RoundedText.module.scss"
 
 const RoundedText = () => {
 
-    const cursorText = "GSAP-Rounded-Text-Cursor-Animation"
-
+    const [cursorText,setCursorText] = useState<string>("Rotating Text")
+    let radius = 100
+    let fontSize = 36
     const rootRef = useRef<HTMLDivElement|null>(null)
     useLayoutEffect(()=>{
         const context = gsap.context(()=>{
 
-            for (let i=0;i<cursorText.length;i++){
-                let rotation = i*(360/cursorText.length)
-                let radius = 100
-                let fontSize = 16
+            for (let i=0;i<cursorText.length+1;i++){
+                let rotation = i*(360/(cursorText.length+1))
                 gsap.set(`.cursorLetter${i}`,{
-                    border:"1px solid white",
-                    x:-fontSize*i + radius*Math.cos(i*2*Math.PI/cursorText.length) + fontSize*cursorText.length/2 - fontSize/2,
-                    y: radius*Math.sin(i*2*Math.PI/cursorText.length),
-                    rotate: (i*(360/cursorText.length))+90
+                    color:"white",
+                    x:-fontSize*i + radius*Math.cos(i*2*Math.PI/(cursorText.length+1)) + fontSize*cursorText.length/2 - fontSize/2,
+                    y: radius*Math.sin(i*2*Math.PI/(cursorText.length+1)),
+                    rotate: (i*(360/(cursorText.length+1)))+90
                 })
                 gsap.fromTo(".cursorText",{
+                    display:"none",
                     rotate:0
                 },{
+                    display:"flex",
                     rotate:360,
                     repeat:-1,
                     ease:"none",
-                    duration:5
+                    duration:10
                 })
             }
 
@@ -35,27 +36,33 @@ const RoundedText = () => {
         return ()=>{
             context.revert()
         }
-    },[])
+    },[cursorText])
+
+    console.log(cursorText+"_")
 
     return (
         <div className={style.Container} ref={rootRef}>
-            <h1>
-                <span>GSAP Rounded Text</span><br/>
-                Cursor Animation
-            </h1>
-            <div className={style.cursor}>
-                <div className={style.cursorInner}>
-                    <div className={`${style.cursorText} cursorText`}>
-                        {cursorText.split("").map((letter,index)=>(
-                            <div
-                            key={`${letter}${index}`}
-                            className={`${style.cursorLetter} cursorLetter${index}`}
-                            >
-                                {letter}
-                            </div>
-                        ))}
+            
+            <div className={style.inputField}>
+                <h1>
+                    write here
+                </h1>
+                <input
+                type="text"
+                value={cursorText}
+                onChange={(e)=>{setCursorText(`${e.target.value}`)}}
+                />
+            </div>
+
+            <div className={`${style.cursorText} cursorText`}>
+                {(cursorText+"-").split("").map((letter,index)=>(
+                    <div
+                    key={`${letter}${index}`}
+                    className={`${style.cursorLetter} cursorLetter${index}`}
+                    >
+                        {letter==" "?"-":letter}
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     );
