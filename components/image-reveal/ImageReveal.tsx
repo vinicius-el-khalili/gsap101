2228f267abea43f5d9ade70f90ee9c18e1b6b013
vini <velkhaliliborsato@gmail.com>
2023-11-19 23:45:56 -0300

    nice

--- components/image-reveal/ImageReveal.tsx
+++ components/image-reveal/ImageReveal.tsx
@@ -1,20 +1,60 @@
 "use client"
+
+import gsap from "gsap";
+import { useLayoutEffect, useRef } from "react";
+
+import asset from "@/public/assets/mountains.jpg"
 import style from "./ImageReveal.module.scss"
 import Image from "next/image";
+
 const ImageReveal = () => {
+
+    const rootRef = useRef<HTMLDivElement|null>(null)
+    useLayoutEffect(()=>{
+        const context = gsap.context(()=>{
+
+            gsap.fromTo(".imageContainer",{
+                opacity:0,
+                scale:1.6
+            },{
+                opacity:1,
+                scale:1
+            })
+            gsap.fromTo(".textContainer",{
+                opacity:0,
+                y:200
+            },{
+                opacity:1,
+                y:0
+            })
+
+        },rootRef)
+    },[])
+
     return (
-        <div className={style.ImageRevealPage}>
-            <h1>IMAGE TRANSITION</h1>
-            <div className={style.imageContainer}>
+
+        <div
+        className={style.ImageRevealPage}
+        ref={rootRef}>
+
+            <h1
+            className={`textContainer ${style.textContainer}`}
+            >
+                IMAGE TRANSITION
+            </h1>
+            <div className={`imageContainer ${style.imageContainer}`}>
+
                 <Image
-                src={"/assets/mountains.jpg"}
+                src={asset}
                 alt=""
                 fill
-                style={{objectFit:"cover"}}
+                style={{objectFit:"contain"}}
                 />
+
             </div>
             
         </div>
+        
     );
 }
  
